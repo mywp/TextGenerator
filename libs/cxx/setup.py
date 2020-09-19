e4c674081e9ca6e5bd87f5a009b857fcef80ce26
Jiajie Zhu <wp8student@live.com>
2020-09-19 15:00:31 +0800

    Update setup.py
    适配macos 10.15.6

--- libs/cxx/setup.py
+++ libs/cxx/setup.py
@@ -1,7 +1,29 @@
+#macbook
+
 from distutils.core import setup
 
 import numpy as np
 from Cython.Build import cythonize
 
 numpy_include = np.get_include()
-setup(ext_modules=cythonize("select_by_kp.pyx"), include_dirs=[numpy_include])
+
+#修改后macos 10.15.6
+from Cython.Distutils import build_ext
+#修改后macos 10.15.6
+class CustomBuildExtCommand(build_ext):
+    """build_ext command for use when numpy headers are needed."""
+    def run(self):
+
+        # Import numpy here, only when headers are needed
+        import numpy
+
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+
+        # Call original build_ext command
+        build_ext.run(self)
+#原有项目
+#setup(ext_modules=cythonize("select_by_kp.pyx"), include_dirs=[numpy_include])
+
+#修改后macos 10.15.6
+setup(cmdclass = {'build_ext': CustomBuildExtCommand},include_dirs = [np.get_include()],ext_modules=cythonize("select_by_kp.pyx"))
